c26b92cf22abc92700a2a67673c748328b7208cc
snowPu <sukanya1993@gmail.com>
2020-04-21 11:42:04 +0200

    debug batchnorm

--- image_downscaling/conv_autoencoder.py
+++ image_downscaling/conv_autoencoder.py
@@ -184,19 +184,19 @@ class AutoEncoder:
         # encoder
 
         conv1 = Conv2D(16, (3, 3), activation='relu', padding='same')(inputs)
-        conv1 = BatchNormalization(conv1)
+        conv1 = BatchNormalization()(conv1)
         conv1 = Conv2D(16, (3, 3), activation='relu', padding='same')(conv1)
-        conv1 = BatchNormalization(conv1)
+        conv1 = BatchNormalization()(conv1)
         pool1 = MaxPooling2D((2, 2), padding='same')(conv1) # 128
         conv2 = Conv2D(32, (3, 3), activation='relu', padding='same')(pool1)
-        conv2 = BatchNormalization(conv2)
+        conv2 = BatchNormalization()(conv2)
         conv2 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv2)
-        conv2 = BatchNormalization(conv2)
+        conv2 = BatchNormalization()(conv2)
         pool2 = MaxPooling2D((2, 2), padding='same')(conv2) # 64
         conv3 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool2)
-        conv3 = BatchNormalization(conv3)
+        conv3 = BatchNormalization()(conv3)
         conv3 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv3)
-        conv3 = BatchNormalization(conv3)
+        conv3 = BatchNormalization()(conv3)
         drop3 = Dropout(0.5)(conv3)
         pool3 = MaxPooling2D((2, 2), padding='same')(drop3) # 32
 
@@ -210,16 +210,16 @@ class AutoEncoder:
         up5 = Conv2D(64, (3, 3), activation='relu', padding='same')(Conv2DTranspose((2, 2))(drop4)) # 64
         merge5 = concatenate([drop3, up5], axis=3)
         conv5 = Conv2D(64, (3, 3), activation='relu', padding='same')(merge5)
-        conv5 = BatchNormalization(conv5)
+        conv5 = BatchNormalization()(conv5)
         conv5 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv5)
-        conv5 = BatchNormalization(conv5)
+        conv5 = BatchNormalization()(conv5)
 
         up6 = Conv2D(32, (3, 3), activation='relu', padding='same')(Conv2DTranspose((2, 2))(conv5)) # 128
         merge6 = concatenate([conv2, up6], axis=3)
         conv6 = Conv2D(32, (3, 3), activation='relu', padding='same')(merge6)
-        conv6 = BatchNormalization(conv6)
+        conv6 = BatchNormalization()(conv6)
         conv6 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv6)
-        conv6 = BatchNormalization(conv6)
+        conv6 = BatchNormalization()(conv6)
         conv6 = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(conv6)
 
 
