df90e9fa1738b53f195697c16de5fbf89d19d514
snowPu <sukanya1993@gmail.com>
2020-04-28 08:29:21 +0200

    added batch norm to bottleneck

--- image_downscaling/conv_autoencoder_test.py
+++ image_downscaling/conv_autoencoder_test.py
@@ -4,6 +4,7 @@ import os
 import cv2
 from conv_autoencoder import *
 from keras import backend as K
+import lpf_ss
 
 #
 # loss = 'perceptual_ssim'
@@ -11,7 +12,7 @@ from keras import backend as K
 
 # model_folder = 'weights/weights_' + loss + '_' + optimizer
 model_folder_name = 'weights_perceptual_ssim_nadam_500_0.0002_0.9_0.999_1587746299.670421'
-epochs = 'final'
+epochs = '00000420'
 model_epoch_name = 'ae_weights_' + epochs
 model_folder = 'weights/' + model_folder_name
 output_folder = 'autoencoder/' + model_folder_name + '_' + epochs
@@ -30,55 +31,61 @@ test_folder = '../../mipmap_LPF_SS'
 
 mipmap_test_io = [
     {'input': test_folder + '/256x256',
-     'output': test_folder + '/128x128/' + output_folder},
+     'output': test_folder + '/128x128/' + output_folder,
+     'method': 'autoencoder'},
     {'input': test_folder + '/128x128/' + output_folder,
-     'output': test_folder + '/64x64/' + output_folder},
+     'output': test_folder + '/64x64/' + output_folder,
+     'method': 'autoencoder'},
     {'input': test_folder + '/64x64/' + output_folder,
-     'output': test_folder + '/32x32/' + output_folder},
+     'output': test_folder + '/32x32/' + output_folder,
+     'method': 'autoencoder'},
     {'input': test_folder + '/32x32/' + output_folder,
-     'output': test_folder + '/16x16/' + output_folder},
+     'output': test_folder + '/16x16/' + output_folder,
+     'method': 'autoencoder'},
     {'input': test_folder + '/16x16/' + output_folder,
-     'output': test_folder + '/8x8/' + output_folder},
+     'output': test_folder + '/8x8/' + output_folder,
+     'method': 'autoencoder'},
     {'input': test_folder + '/8x8/' + output_folder,
-     'output': test_folder + '/4x4/' + output_folder},
-    # {'input': test_folder + '/4x4/' + output_folder,
-    #  'output': test_folder + '/2x2/' + output_folder},
-    # {'input': test_folder + '/2x2/' + output_folder,
-    #  'output': test_folder + '/1x1/' + output_folder}
+     'output': test_folder + '/4x4/' + output_folder,
+     'method': 'autoencoder'},
+    {'input': test_folder + '/4x4/' + output_folder,
+     'output': test_folder + '/2x2/' + output_folder,
+     'method': 'lpf_ss'},
+    {'input': test_folder + '/2x2/' + output_folder,
+     'output': test_folder + '/1x1/' + output_folder,
+     'method': 'lpf_ss'}
 ]
 
 
 for mipmap_entry in mipmap_test_io:
-
-
     test_input_path = mipmap_entry["input"]
     test_output_path = mipmap_entry["output"]
-    # test_image = 'rcd549fe5t.png'
 
     if not os.path.exists(test_output_path):
         os.mkdir(test_output_path)
 
-
     test_images = os.listdir(test_input_path)
-
 
     for test_image in test_images:
         input_image = cv2.imread(os.path.join(test_input_path, test_image))
-        input_np = np.array(input_image) / 255
-        input_np = input_np.reshape(1, input_np.shape[0], input_np.shape[1], input_np.shape[2])
 
- 
+        if (mipmap_entry["method"] == 'autoencoder'):
+            input_np = np.array(input_image) / 255
+            input_np = input_np.reshape(1, input_np.shape[0], input_np.shape[1], input_np.shape[2])
+
+            y = model.predict(input_np)
+
+            y_image = np.uint8((y[0] * 255))
+        else:
+            y_image = lpf_ss.downscale_by_two(input_image)
+
+
         # output_image_shape = (int(input_image.shape[0]/ 2), int(input_image.shape[1] / 2), input_image.shape[2])
         #
         # print(output_image_shape)
 
         # x = encoder.predict(input_np)
         # y = decoder.predict(x)
-
-        y = model.predict(input_np)
-
-        y_image = np.uint8((y[0] * 255))
-
         # print(y_image)
 
         output_image_path = test_output_path + '/' +  test_image
---
+++ image_downscaling/lpf_ss.py
@@ -0,0 +1,29 @@
+import cv2
+import skimage.measure
+import numpy as np
+
+# Gaussian Filter Kernel Size 3
+
+def gaussian_filter_3(img):
+    result = cv2.GaussianBlur(img, (3, 3), 0)
+    return result
+
+# Gaussian Filter Kernel Size 5
+
+def gaussian_filter_5(img):
+    result = cv2.GaussianBlur(img, (5, 5), 0)
+    return result
+
+
+
+# Take median of four pixels
+
+def sample_median(img):
+    result = skimage.measure.block_reduce(img, (2,2,1), np.median)
+    return result
+
+
+def downscale_by_two(img):
+    lpf_image = gaussian_filter_3(img)
+    downscaled = sample_median(lpf_image)
+    return downscaled
